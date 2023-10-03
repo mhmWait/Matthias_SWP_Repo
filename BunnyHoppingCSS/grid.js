39cd867fdd47c1aaa50934b9c5a3aba9f6bcc7c5
mhmWait <FIE22213@spengergasse.at>
2023-10-03 15:48:57 +0200

    FERTIG MIT HASE

--- BunnyHoppingCSS/grid.js
+++ BunnyHoppingCSS/grid.js
@@ -1,19 +1,22 @@
 var bunny = document.querySelector("#bunny");
 var root = document.querySelector("#root");
 
-wann = 0;
-
-function hoop(row, column) {
-    bunny.style.gridRowStart = row;
-    bunny.style.gridColumnStart = column;
-}
 function abc() {
-    for (i = 1; i < 8; i++) {
-        wann += 1000;
-
-        for (k = 1; k < 8; k++) {
-            wann += 1000;
-            setTimeout(hoop(i, k), wann);
+    a = 1;
+    b = 1;
+    const avs = setInterval(function () {
+        a++;
+        console.log(a % 7);
+        if (a % 7 != 0) {
+            bunny.style.gridColumnStart = a % 7;
         }
-    }
+        if (a % 7 == 0) {
+            b++;
+            bunny.style.gridColumnStart = 1;
+        }
+        if (b == 8 && a % 7 == 6) {
+            clearInterval(avs);
+        }
+        bunny.style.gridRowStart = b;
+    }, 200);
 }
