d858ce2558fb436173a63fef7934a40c89cfed88
mhmWait <FIE22213@spengergasse.at>
2024-10-07 15:42:00 +0200

    Sudoku Erstmaliges Rüberkopieren

--- ExpressTesten/app.js
+++ ExpressTesten/app.js
@@ -1,13 +1,21 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3000;
-const prisma = require('./lib/db');
-app.set('view engine', 'ejs');
-app.set('views', 'views');
-app.use('/', express.static('static'));
+const prisma = require("./lib/db");
+app.set("view engine", "ejs");
+app.set("views", "views");
+app.use("/", express.static("static"));
 
-app.get('/', async (req, res) => {
-    res.render('index', { zoos: await prisma.zoo.findMany() });
+app.get("/", async (req, res) => {
+    res.render("index", { zoos: await prisma.zoo.findMany() });
+});
+
+app.get("/zoo/:id", async (req, res) => {
+    res.render("abteilungen", {
+        abteilungen: await prisma.abteilungen.findMany({
+            select: { id: res.params.id },
+        }),
+    });
 });
 
 app.listen(port, () => {
