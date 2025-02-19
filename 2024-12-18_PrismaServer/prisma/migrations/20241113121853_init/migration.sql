473d21f6b1b18ce8c6ce549dbcddb015ac5a0dde
mhmWait <FIE22213@spengergasse.at>
2025-02-19 13:15:07 +0100

    dadad

--- 2024-12-18_PrismaServer/prisma/migrations/20241113121853_init/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "Drink" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "name" TEXT NOT NULL,
-    "ml" REAL NOT NULL,
-    "price" REAL NOT NULL,
-    "alcohol" REAL NOT NULL
-);
