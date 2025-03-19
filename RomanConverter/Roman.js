177dd377dba467d1035c47e9f5ccdc3de225a9ca
mhmWait <FIE22213@spengergasse.at>
2025-03-19 12:39:05 +0100

    jo jo

---
+++ RomanConverter/Roman.js
@@ -0,0 +1,18 @@
+const romans = require("romans");
+
+document.getElementById
+
+
+function Numberise(input) {
+    return romans.deromanize(input);
+}
+
+function Romanise(input) {
+    return romans.romanize(input);
+}
+
+
+function main() {
+    
+}
+main();
